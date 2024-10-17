40078d31fa10d56a885452839c60d5440d15a152
marioSim2002 <wajd.simaan@gmail.com>
2024-10-17 17:15:38 +0300

    test-unit

--- src/main/java/com/example/solvesphere/TestUnit/SecurityTests.java
+++ src/main/java/com/example/solvesphere/TestUnit/SecurityTests.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class SecurityTests {
     private final String PASSWORD_FOR_TEST = "tstPass123";
-    private PasswordHasher hasher = new PasswordHasher();
+    private final PasswordHasher hasher = new PasswordHasher();
     @Test
     public void testPassHahsing(){
         assert !hasher.hashPassword(PASSWORD_FOR_TEST).equals(PASSWORD_FOR_TEST);
