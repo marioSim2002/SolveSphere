559b2ded46e44499818ad49246f82db1367a6b80
marioSim2002 <wajd.simaan@gmail.com>
2024-10-20 19:12:55 +0300

    filtering problems via DB and GUI

--- src/main/java/com/example/solvesphere/DBQueries/ProblemQueries.java
+++ src/main/java/com/example/solvesphere/DBQueries/ProblemQueries.java
@@ -4,6 +4,5 @@ public class ProblemQueries {
     public static final String SELECT_PROBLEMS_BY_INTEREST = "SELECT * FROM problems WHERE category IN (?)";
     public static final String SELECT_PROBLEM_BY_ID = "SELECT * FROM problems WHERE id = ?";
     public static final String SEARCH_PROBLEMS = "SELECT * FROM problems WHERE title LIKE ? OR description LIKE ?";
-
     public static final String SELECT_ALL_PROBLEMS ="SELECT * FROM problems";
 }
