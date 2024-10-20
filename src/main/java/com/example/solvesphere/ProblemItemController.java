600f399b8d14dfdbf426bd0ffcbb3f204d365e58
marioSim2002 <wajd.simaan@gmail.com>
2024-10-20 12:00:27 +0300

    problem item-gui

--- src/main/java/com/example/solvesphere/ProblemItemController.java
+++ src/main/java/com/example/solvesphere/ProblemItemController.java
@@ -1,2 +1,19 @@
-package com.example.solvesphere;public class ProblemItemController {
+package com.example.solvesphere;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ProblemItemController {
+    @FXML private Label problemTitle;
+    @FXML private Label problemDescription;
+
+    public void setProblemData(String title, String description) {
+        this.problemTitle.setText(title);
+        this.problemDescription.setText(description);
+    }
+
+    @FXML
+    private void onDetailsClick() {
+        // logic to show details
+    }
 }
